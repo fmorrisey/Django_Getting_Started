bf4c3558eb3dffefa86a1aeaa9a999a2ec696345
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-12 11:29:14 -0600

    Removed key to local file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,3 +1,5 @@
+import secret as secret
+
 """
 Django settings for mysite project.
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
